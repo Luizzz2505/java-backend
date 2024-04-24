e367d8beba34b254c09ebec292ffc235d417368e
Luiz Francisco <luizcostaneto2505@gmail.com>
2024-04-23 21:55:48 -0300

    teste sozinho

---
+++ src/Teste.java
@@ -0,0 +1,6 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Teste para saber se consigo levar este documento para o github sozinho");
+    }
+}
+
